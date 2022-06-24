d2639c54ca570523042ae9939bdaa2c8c0246057
raihanpf22 <raihanpambagyo@.com>
2022-06-24 18:32:22 +0700

    Dasar JS

---
+++ variabeltipedata.js
@@ -0,0 +1,23 @@
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai nama depan Anda.
+ *  - lastName: bertipe data string, dengan nilai nama belakang Anda.
+ *  - age: bertipe data number, dengan nilai umur Anda.
+ *  - isMarried: bertipe data boolean, dengan bebas Anda tentukan.
+ */
+
+// TODO
+const firstName = "raihan"
+const lastName = "fadhila"
+const age = 21
+const isMarried = false
+
+/**
+ * Jangan hapus kode di bawah ini
+ */
+console.log(firstName+" "+lastName+"\n"+age+"\n"+isMarried)
+
+//  module.exports = {
+//     firstName, lastName, age, isMarried,
+//   };
